17c114fc0185498422cd0509bf8fc08ca082f4fc
Chris Herr <cherr2@andrew.cmu.edu>
2025-08-28 16:09:11 -0400

    Update install.sh

--- skilling-continuation-labs/hardenining-kubernetes-pod-security/lab/skillshub/k3s-install/install.sh
+++ skilling-continuation-labs/hardenining-kubernetes-pod-security/lab/skillshub/k3s-install/install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Copyright 2025 Carnegie Mellon University.
+# Released under a MIT (SEI)-style license, please see LICENSE.md in the project 
+# root or contact permission@sei.cmu.edu for full terms.
+
 #!/bin/sh
 set -e
 set -o noglob
